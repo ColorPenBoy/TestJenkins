b18a81138ecd00dd23427835e75e2f57dc58e18b
张强 <zq252125@163.com>
2016-03-04 14:29:23 +0800

    Initial Commit

---
+++ TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by 张强 on 16/3/4.
+//  Copyright © 2016年 ColorPen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestJenkins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestJenkins
+//
+//  Created by 张强 on 16/3/4.
+//  Copyright © 2016年 ColorPen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
