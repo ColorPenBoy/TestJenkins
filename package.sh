26f31dfc48de4aa2920159c2b0ece9c86ec7c787
张强 <zq252125@163.com>
2016-03-04 19:20:50 +0800

    change

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 pod install --verbose --no-repo-update &&  \
-xcodebuild -workspace LZAlbum.xcworkspace -scheme LZAlbum archive -archivePath ./build/LZAlbum.xcarchive && \
-rm -rf ./build/LZAlbum.ipa && \
-xcodebuild -exportArchive -exportFormat ipa -archivePath build/LZAlbum.xcarchive -exportPath build/LZAlbum.ipa && \
-fir p build/LZAlbum.ipa -T 0823a5646b2db2b9eab469866c41bce3
+xcodebuild -workspace TestJenkins.xcworkspace -scheme TestJenkins archive -archivePath ./build/TestJenkins.xcarchive && \
+rm -rf ./build/TestJenkins.ipa && \
+xcodebuild -exportArchive -exportFormat ipa -archivePath build/TestJenkins.xcarchive -exportPath build/TestJenkins.ipa && \
+fir p build/TestJenkins.ipa -T 0823a5646b2db2b9eab469866c41bce3
