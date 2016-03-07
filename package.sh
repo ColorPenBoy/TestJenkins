303f194c0b68638fc1960f2a28b92f8062b422e0
张强 <zq252125@163.com>
2016-03-07 16:14:31 +0800

    change shell

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 
 # 使用pod管理项目的，先导入依赖库，未使用pod、或者项目依赖库都存在的，可以注释掉
-#pod install --verbose --no-repo-update &&  \
+pod install --verbose --no-repo-update &&  \
 
 # 生成 XXX.xcarchive
 xcodebuild -workspace TestJenkins.xcworkspace -scheme TestJenkins archive -archivePath ./build/TestJenkins.xcarchive && \
@@ -9,7 +9,7 @@ xcodebuild -workspace TestJenkins.xcworkspace -scheme TestJenkins archive -archi
 rm -rf ./build/TestJenkins.ipa && \
 
 # 使用生成的 XXX.xcarchive -> (XXX表示项目名称) 生成 XXX.ipa
-xcodebuild -exportArchive -exportFormat ipa -archivePath build/TestJenkins.xcarchive -exportPath build/TestJenkins.ipa -exportProvisioningProfile comvsoutestjenkinsappstore && \
+xcodebuild -exportArchive -exportFormat ipa -archivePath build/TestJenkins.xcarchive -exportPath build/TestJenkins.ipa && \
 
 #xcodebuild -exportArchive -exportFormat IPA \
 #-archivePath /Path/To/Archive/name.xcarchive \
